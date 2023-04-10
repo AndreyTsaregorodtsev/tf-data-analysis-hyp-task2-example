abaa71a5c41f16bc550b1cb200b32aa1de4ed4e1
AndreyTsaregorodtsev <71283873+AndreyTsaregorodtsev@users.noreply.github.com>
2023-04-10 23:06:45 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,12 +1,12 @@
 import pandas as pd
 import numpy as np
-from scipy.stats import cramervonmises_2samp
+from scipy.stats import ks_2samp
 
 
 chat_id = 297386717 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    if(cramervonmises_2samp(x,y).pvalue < 0.01):
+    if(ks_2samp(x,y).pvalue < 0.01):
       return False
     else:
       return True
