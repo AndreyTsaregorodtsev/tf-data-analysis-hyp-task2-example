d0e93b99b01dad45504c2c22e924cfc9af446842
AndreyTsaregorodtsev <71283873+AndreyTsaregorodtsev@users.noreply.github.com>
2023-04-11 00:12:11 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,12 +1,12 @@
 import pandas as pd
 import numpy as np
-from scipy.stats import ks_2samp
+from statsmodels.stats.weightstats import ztest
 
 
 chat_id = 297386717 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    if(ks_2samp(x,y).pvalue < 0.01):
-      return False
-    else:
+    if(ztest(x,y)[1] < 0.01):
       return True
+    else:
+      return False
